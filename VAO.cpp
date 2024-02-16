6d6e35ba2949d966e4f452bd2633db093b36d485
Alfred-Jijo <alfredjijo28@gmail.com>
2024-02-16 13:47:49 +0000

    Added more GLSL shaders

--- VAO.cpp
+++ VAO.cpp
@@ -1,34 +1,29 @@
 #include"VAO.h"
 
 // Constructor that generates a VAO ID
-VAO::VAO()
-{
+VAO::VAO() {
 	glGenVertexArrays(1, &ID);
 }
 
-// Links a VBO to the VAO using a certain layout
-void VAO::LinkVBO(VBO& VBO, GLuint Layout)
-{
+// Links a VBO Attribute such as a position or color to the VAO
+void VAO::LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset) {
 	VBO.Bind();
-	glVertexAttribPointer(Layout, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glEnableVertexAttribArray(Layout);
+	glVertexAttribPointer(layout, numComponents, type, GL_FALSE, stride, offset);
+	glEnableVertexAttribArray(layout);
 	VBO.Unbind();
 }
 
 // Binds the VAO
-void VAO::Bind()
-{
+void VAO::Bind() {
 	glBindVertexArray(ID);
 }
 
 // Unbinds the VAO
-void VAO::Unbind()
-{
+void VAO::Unbind() {
 	glBindVertexArray(0);
 }
 
 // Deletes the VAO
-void VAO::Delete()
-{
+void VAO::Delete() {
 	glDeleteVertexArrays(1, &ID);
 }
--- VAO.h
+++ VAO.h
@@ -1,22 +1,24 @@
-#pragma once
+#ifndef VAO_CLASS_H
+#define VAO_CLASS_H
 
 #include<glad/glad.h>
 #include"VBO.h"
 
-
 class VAO {
-	public:
-		// ID reference for the Vertex Array Object
-		GLuint ID;
-		// Constructor that generates a VAO ID
-		VAO();
+public:
+	// ID reference for the Vertex Array Object
+	GLuint ID;
+	// Constructor that generates a VAO ID
+	VAO();
 
-		// Links a VBO to the VAO using a certain layout
-		void LinkVBO(VBO& VBO, GLuint layout);
-		// Binds the VAO
-		void Bind();
-		// Unbinds the VAO
-		void Unbind();
-		// Deletes the VAO
-		void Delete();
+	// Links a VBO Attribute such as a position or color to the VAO
+	void LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
+	// Binds the VAO
+	void Bind();
+	// Unbinds the VAO
+	void Unbind();
+	// Deletes the VAO
+	void Delete();
 };
+
+#endif
